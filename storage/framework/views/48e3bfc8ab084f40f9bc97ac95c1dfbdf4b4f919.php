c8a507242639ede28594d03c65c910f838842f2a
bahy711 <80623614+bahy711@users.noreply.github.com>
2022-05-08 17:26:23 +0200

    uploading

---
+++ storage/framework/views/48e3bfc8ab084f40f9bc97ac95c1dfbdf4b4f919.php
@@ -0,0 +1,13 @@
+<tr>
+<td class="header">
+<a href="<?php echo e($url); ?>" style="display: inline-block;">
+<?php if(trim($slot) === 'Laravel'): ?>
+
+<?php else: ?>
+<?php echo e($slot); ?>
+
+<?php endif; ?>
+</a>
+</td>
+</tr>
+<?php /**PATH C:\xampp\htdocs\Gary_app\vendor\laravel\framework\src\Illuminate\Mail/resources/views/html/header.blade.php ENDPATH**/ ?>
